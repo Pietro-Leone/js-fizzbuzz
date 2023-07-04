54634c84a3ce96e309b0a1ab7f1bd9d81d0741ee
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-04 17:12:43 +0200

    Cambiamenti minori

--- js/main.js
+++ js/main.js
@@ -8,29 +8,29 @@ for (let i = 1; i <= 100; i++) {
     let singleBlock = document.createElement("div");
     singleBlock.classList.add("block");
     singleBlock.innerHTML = i.toString();
+    blockContainer.append(singleBlock);
 
     // Blocco IF 
     if (i % 3 === 0) {  //Controllo se i è divisibile per 3
         if (i % 5 === 0) {  //Controllo se i è divisibile anche per 5
             console.log(i + " " + "FizzBuzz")   //Se è divisibile per entrambi stampo FizzBuzz
-            singleBlock.classList.add("ms-color-red");
-            singleBlock.innerHTML = "FizzBuzz";
+            singleBlock.style.backgroundColor = "#f0466f";
+            singleBlock.innerHTML = "Fizz Buzz";
         }
         else { //se è divisibile solo per 3 stampo solo Fizz
             console.log(i + " " + "Fizz")
-            singleBlock.classList.add("ms-color-green");
+            singleBlock.style.backgroundColor = "#0cd6a1";
             singleBlock.innerHTML = "Fizz";
         }
     }
     else if (i % 5 === 0) { //Controllo se i è divisibile per 5
         console.log(i + " " + "Buzz") //Se è divisibile per 5 stampo Buzz
-        singleBlock.classList.add("ms-color-yellow");
+        singleBlock.style.backgroundColor = "#ffd166";
         singleBlock.innerHTML = "Buzz";
     }
     else { //Se non è divisibile n'è per 3 n'è per 5 allora stampo i
         console.log(i)
-        singleBlock.classList.add("ms-color-blu");
+        singleBlock.style.backgroundColor = "#1389b2";
     }
 
-    blockContainer.append(singleBlock);
 }
